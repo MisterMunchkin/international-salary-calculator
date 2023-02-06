6734f65e1873dab5a5d5a93f428267f82d2f45c1
Robin Dalmy Tubungbanua <MisterMunchkin@users.noreply.github.com>
2023-02-06 18:39:53 +0800

    removed font awesome icons and used primeng icons instead

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons/faLinkedin';
-import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub'
 
 @Component({
   selector: 'app-footer',
@@ -8,8 +6,6 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub'
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-  faLinkedin = faLinkedin;
-  faGithub = faGithub;
 
   constructor() { }
 
