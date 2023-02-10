921ac0174c11f81d7fe42e7fb1746c45219034e3
Robin Dalmy Tubungbanua <MisterMunchkin@users.noreply.github.com>
2023-02-10 16:50:28 +0800

    Fixed issue where responsive css such as md:* does not work. https://stackoverflow.com/a/65583198/5395605

--- src/scripts/purgecss.js
+++ src/scripts/purgecss.js
@@ -3,43 +3,49 @@ const fs = require('fs');
 const path = require('path');
 
 // find the styles css file
-const files = getFilesFromPath('./dist/international-salary-calculator-angular', '.css');
+const purgeCSSModulePath = './node_modules/purgecss/bin/purgecss.js';
+const buildOutputPath = './dist/international-salary-calculator-angular';
+const purgeCSSConfigFilePath = './purgecss.config.js';
+
+const files = getFilesFromPath(buildOutputPath, '.css');
 let data = [];
 
 if (!files && files.length <= 0) {
-console.log('cannot find style files to purge');
-return;
+  console.log('cannot find style files to purge');
+  return;
 }
 
 for (let f of files) {
-// get original file size
-const originalSize = getFilesizeInKiloBytes('./dist/international-salary-calculator-angular/' + f) + 'kb';
-var o = { file: f, originalSize: originalSize, newSize: '' };
-data.push(o);
+  // get original file size
+  const originalSize = getFilesizeInKiloBytes(`${buildOutputPath}/` + f) + 'kb';
+  var o = { file: f, originalSize: originalSize, newSize: '' };
+  data.push(o);
 }
-console.log('Run PurgeCSS...');
-exec(
-'./node_modules/purgecss/bin/purgecss.js -css dist/international-salary-calculator-angular/*.css --content dist/international-salary-calculator-angular/index.html dist/international-salary-calculator-angular/*.js -o dist/international-salary-calculator-angular/',
-function (error, stdout, stderr) {
-console.log('PurgeCSS done');
-console.log();
 
-for (let d of data) {
-// get new file size
-const newSize = getFilesizeInKiloBytes('./dist/international-salary-calculator-angular/' + d.file) + 'kb';
-d.newSize = newSize;
-}
-console.table(data);
-}
-);
+console.log('Run PurgeCSS...');
+
+const executeCommand = `${purgeCSSModulePath} --config ${purgeCSSConfigFilePath}`;
+
+exec(executeCommand,
+function (error, stdout, stderr) {
+  console.log('PurgeCSS done');
+  console.log();
+
+  for (let d of data) {
+    // get new file size
+    const newSize = getFilesizeInKiloBytes(`${buildOutputPath}/` + d.file) + 'kb';
+    d.newSize = newSize;
+  }
+  console.table(data);
+});
 
 function getFilesizeInKiloBytes(filename) {
-var stats = fs.statSync(filename);
-var fileSizeInBytes = stats.size / 1024;
-return fileSizeInBytes.toFixed(2);
+  var stats = fs.statSync(filename);
+  var fileSizeInBytes = stats.size / 1024;
+  return fileSizeInBytes.toFixed(2);
 }
 
 function getFilesFromPath(dir, extension) {
-let files = fs.readdirSync(dir);
-return files.filter((e) => path.extname(e).toLowerCase() === extension);
+  let files = fs.readdirSync(dir);
+  return files.filter((e) => path.extname(e).toLowerCase() === extension);
 }
