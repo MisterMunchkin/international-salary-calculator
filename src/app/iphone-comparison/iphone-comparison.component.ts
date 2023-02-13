c2ebfa53ef1c3ac3135cbce16bfbb3ed04001531
Robin Dalmy Tubungbanua <MisterMunchkin@users.noreply.github.com>
2023-02-13 19:14:22 +0800

    fixed issues with iPhone comparison component - changed cound't find iPhone prices format - using getters and setters so that updates from the currency forms will update the iPhone comparison - updated component for better maintainability

--- src/app/iphone-comparison/iphone-comparison.component.ts
+++ src/app/iphone-comparison/iphone-comparison.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { iPhonePricesAvgRentData } from '../static-data/iphone-prices-avg-rent-data';
 import { iPhonePricesAvgRent } from '../interfaces/iphone-prices-avg-rent';
 import { getCurrencySymbol } from '@angular/common';
@@ -10,9 +10,38 @@ import Fuse from 'fuse.js';
   styleUrls: ['./iphone-comparison.component.scss']
 })
 //This should just be an independent component where the user can select countries from our data set to compare.
-export class IphoneComparisonComponent implements OnInit {
-  @Input() toCountryName?: string;
-  @Input() fromCountryName?: string;
+export class IphoneComparisonComponent {
+  private _toCurrencyName?: string;
+
+  @Input()
+  set toCurrencyName(value: string) {
+    this._toCurrencyName = value;
+
+    const countryName = this.searchCountryByFuse(value);
+    this.setToComparison(countryName);
+
+    console.log(value);
+    console.log(this.toCountryComparison);
+  }
+  get toCurrencyName() : string {
+    return this._toCurrencyName ?? '';
+  }
+
+  private _fromCurrencyName?: string;
+
+  @Input()
+  set fromCurrencyName(value: string) {
+    this._fromCurrencyName = value;
+
+    const countryName = this.searchCountryByFuse(value);
+    this.setFromComparison(countryName);
+
+    console.log(value);
+    console.log(this.fromCountryComparison);
+  }
+  get fromCurrencyName() : string {
+    return this._fromCurrencyName ?? '';
+  }
 
   iPhonePricesAvgRent: Array<iPhonePricesAvgRent> = [];
 
@@ -39,6 +68,7 @@ export class IphoneComparisonComponent implements OnInit {
     this.iPhonePricesAvgRent = JSON.parse(JSON.stringify(iPhonePricesAvgRentData.iphonePricesAvgRent));
 
     this.countries = this.iPhonePricesAvgRent.map(e => e.country);
+    this.countries.push('Not Applicable'); //for when no prices available for specific country
 
     this.iPhonePriceByCountryHash = Object.fromEntries(
       this.iPhonePricesAvgRent.map(e => [e.country, e.iphonePrice])
@@ -47,21 +77,29 @@ export class IphoneComparisonComponent implements OnInit {
     this.fuse = new Fuse(this.countries);
   }
 
-  ngOnInit() {
-    //fuzzy search for country name in data set based on currency description
-    if (this.toCountryName) {
-      const toFuseResults = this.fuse.search(this.toCountryName);
-      const toCountryResult = toFuseResults[0]?.item ?? '';
-      this.toCountryComparison.country =  toCountryResult;
-      this.toCountryComparison.iphonePrice = this.iPhonePriceByCountryHash[toCountryResult];
+  searchCountryByFuse(currencyName: string) : string {
+    if (!currencyName) {
+      return 'Not Applicable';
     }
 
-    if (this.fromCountryName) {
-      const fromFuseResults = this.fuse.search(this.fromCountryName);
-      const fromCountryResult = fromFuseResults[0]?.item ?? '';
-      this.fromCountryComparison.country = fromCountryResult;
-      this.fromCountryComparison.iphonePrice = this.iPhonePriceByCountryHash[fromCountryResult];
-    }
+    const fuseResults = this.fuse.search(currencyName);
+    const countryResult = fuseResults[0]?.item ?? 'Not Applicable';
+
+    return countryResult;
+  }
+
+  setToComparison(countryName: string) {
+    this.toCountryComparison = {
+      country: countryName,
+      iphonePrice: this.iPhonePriceByCountryHash[countryName] ?? undefined
+    };
+  }
+
+  setFromComparison(countryName: string) {
+    this.fromCountryComparison = {
+      country: countryName,
+      iphonePrice: this.iPhonePriceByCountryHash[countryName] ?? undefined
+    };
   }
 
   changeSelection() {
